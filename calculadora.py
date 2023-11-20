7e061687ac59e98d1f5837420b25cf37667aadf4
codingIA-a <149007043+codingIA-a@users.noreply.github.com>
2023-11-20 16:06:30 -0300

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,6 +1,6 @@
 #crear una calculadora interactiva que tome una entrada de usuario y realice 
 #una de las cuatro operaciones básicas (suma, resta, multiplicación o división) tomando dos números
-#PAra esto deberá crear al menos 4 funciones: una para cada operación matemática
+#Para esto deberá crear al menos 4 funciones: una para cada operación matemática
 #Y otra que será la función donde se mostrará el menú principal y se realizarán las operaciones 
 #según elija el usuario
 
@@ -19,6 +19,6 @@ def calculadora():
     while True:
         mostrar_menu()
         opcion = int(input('Ingrese el número de la operación deseada: '))
-        
+        #resto del código
 
 calculadora()
